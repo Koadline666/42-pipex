51ca5d74b0f08ca80c49490238143d4ae8dc6567
Anna-selina Fenzl <afenzl@1-B-9.42heilbronn.de>
2022-06-11 17:16:32 +0200

    starts to work xd

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: afenzl <afenzl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 15:14:09 by afenzl            #+#    #+#             */
-/*   Updated: 2022/06/09 16:49:35 by afenzl           ###   ########.fr       */
+/*   Updated: 2022/06/11 17:16:00 by afenzl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_print2(char **str)
 	return(0);
 }
 
-char	*ft_get_path(char **env)
+char	*ft_get_path(char **env, char *cmd)
 {
 	int i;
 	char *path;
@@ -37,21 +37,26 @@ char	*ft_get_path(char **env)
 	i = 0;
 	while(env[i] != NULL)
 	{
-		if (ft_strnstr(env[i], "PATH=",ft_strlen(env[i])) != NULL)
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
 		{
-			path = ft_strnstr(env[i], "PATH=",ft_strlen(env[i])) + 5;
-			split = ft_split(path, ':');
+			split = ft_split(env[i], ':');
+			break ;
 		}
 		i++;
 	}
-	// ft_print2(split);
-	i = 3;
-	// what i would need here is the path + /cmd and then try access
+	if (env == NULL)
+		return ("./");
+	i = 0;
+	printf("the cmd is: %s\n", cmd);
 	while (split[i] != NULL)
 	{
-		if (access(split[i], 0) == 0)
+		path = ft_strjoin(split[i], "/");
+		path = ft_strjoin(path, cmd);
+		printf("the path is: %s\n", path);
+		if (access(path, 0) == 0)
 		{
-			return("did access the right path");
+			printf("accessed this path: %s\n", path);
+			return(path);
 		}
 		i++;
 	}
@@ -62,6 +67,7 @@ int	main(int argc, char **argv, char **env)
 {
 	int	fd[2];
 	int	id;
+	int fd_read_file;
 	char **str;
 	char *path;
 
@@ -78,21 +84,34 @@ int	main(int argc, char **argv, char **env)
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
+		fd_read_file = open(argv[1], O_RDONLY);
+		dup2(fd_read_file, STDIN_FILENO);
+		close(fd_read_file);
 		str = ft_split(argv[2], ' ');
-		// path = "/bin/ls";
-		path = ft_get_path(env);
-		printf("%s", path);
-		return (0);
-		// if (execve(path, str, env) == -1)
-		// {
-		// 	perror("Error:\ncould not execute first command\n");
-		// 	exit(-1);
-		// }
+		path = ft_get_path(env, str[0]);
+		printf("=====>%s\n", path);
+		if (execve(path, str, env) == -1)
+		{
+			perror("Error:\ncould not execute first command\n");
+			exit(-1);
+		}
 	}
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
 	waitpid(0, NULL, 0);
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
+	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
 	ft_printf("PIPED: %s\n", get_next_line(STDIN_FILENO));
 	// ft_printf("the PATH: %s\n", env[3]);
 	// ft_print2(env);
